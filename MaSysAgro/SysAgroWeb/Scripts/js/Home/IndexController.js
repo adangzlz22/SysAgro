e1313c0d3031df27ef17aa632f63271aeab6328e
AdanGonzalezSicma22 <121247751+AdanGonzalezSicma22@users.noreply.github.com>
2023-09-19 19:21:21 -0700

    subiendo codigo para rozoud de home

---
+++ MaSysAgro/SysAgroWeb/Scripts/js/Home/IndexController.js
@@ -0,0 +1,33 @@
+﻿var LayoutControllers = function () {
+    const url = urlGlobal
+
+    const btnCerrarSession = $('#btnCerrarSession');
+
+    var Inicializar = function () {
+        functionListar();
+        console.log(url)
+    }
+    const functionListar = function () {
+        btnCerrarSession.click(function () {
+            obtener();
+        });
+    }
+    const obtener = function () {
+        let parametros = {
+
+        }
+        const options = url + 'postObtenerMenu';
+        axios.post(options, parametros).then(function (response) {
+            const result = response.data;
+
+
+        }).catch(function (error) {
+            console.error(error);
+        });
+    }
+
+
+    return {
+        Inicializar: Inicializar,
+    }
+};
