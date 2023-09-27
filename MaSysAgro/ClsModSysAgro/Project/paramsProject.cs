075b9e00f69d73b87c5ffb3ed0f4113be9f7dc00
AdanGlez <121247751+AdanGonzalezSicma22@users.noreply.github.com>
2023-09-27 12:49:02 -0700

    SUBIENDO AGREGAR PROJECTOS Y UPDETEAR PROJECTOS

--- MaSysAgro/ClsModSysAgro/Project/paramsProject.cs
+++ MaSysAgro/ClsModSysAgro/Project/paramsProject.cs
@@ -11,5 +11,11 @@ namespace ClsModSysAgro.Project
         public int ProjectID { get; set; }
         public string Chip_ID { get; set; }    
         public int ClientID { get; set; }
+        public string ProjectName { get; set; }
+        public decimal Longitud_1 { get; set; }
+        public decimal Latitud_1 { get; set; }
+        public decimal Longitud_2 { get; set; }
+        public decimal Latitud_2 { get; set; }
+        public int Activo { get; set; } 
     }
 }
