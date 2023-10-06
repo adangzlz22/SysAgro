fcb58ae7b3a63eb226cca7f2f7ba72eb4be15994
lrouzaud <rouzaud1285@gmail.com>
2023-10-05 21:06:19 -0700

    agregar poligono al proyecto

--- MaSysAgro/ClsModSysAgro/Project/resultProject.cs
+++ MaSysAgro/ClsModSysAgro/Project/resultProject.cs
@@ -16,5 +16,6 @@ namespace ClsModSysAgro.Project
         public decimal Longitud_2 { get; set; }
         public decimal Latitud_2 { get; set; }
         public int Activo { get; set; }
+        public string Cordenadas { get; set; }
     }
 }
